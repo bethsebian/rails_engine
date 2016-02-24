1f0fba4e9dc0301b6474e05cb4deb1813de6611f
Beth Sebian <esebian@gmail.com>
2016-02-24 10:11:23 -0700

    Update invoices_helper to deslug queries, aka 'rejected-twice' bcms 'rejected twice'

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,5 +1,7 @@
 module InvoicesHelper
 	def self.finder(params, query_hash)
+		deslug_query(query_hash)
+
 		if query_hash.present? && params[:id] == "find"
 			Invoice.find_by(query_hash)
 		elsif query_hash.present? && params[:id] == "find_all"
@@ -10,5 +12,10 @@ module InvoicesHelper
 			Invoice.find(params[:id])
 		end
 	end
-end
 
+	def self.deslug_query(query_hash)
+		if query_hash.present?
+			query_hash[query_hash.keys[0]] = query_hash.values[0].gsub(/-/, " ")
+		end
+	end
+end
