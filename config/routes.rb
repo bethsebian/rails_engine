cddec85c811c2306a4dcde0c9166c127851f4402
Beth Sebian <esebian@gmail.com>
2016-02-24 00:24:20 -0700

    Finish invoice items for tests, incl find, find_all, rand

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :invoices, only: [:index, :show], defaults: { format: 'json' }
+      resources :invoice_items, only: [:index, :show], defaults: { format: 'json' }
     end
   end
 end
--- spec/requests/api/v1/invoice_items_spec.rb
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "InvoiceItems API" do
-  attr_reader :i_1, :i_2, :i_3
+  attr_reader :ii_1, :ii_2, :ii_3, :ii_4, :ii_5, :ii_6, :ii_7
 
   before(:each) do
-		ii_1 = create(:invoice_item, item_id: 1, invoice_id: 1, quantity: 1)
-		ii_2 = create(:invoice_item, item_id: 2, invoice_id: 1, quantity: 1)
-		ii_3 = create(:invoice_item, item_id: 3, invoice_id: 1, quantity: 1)
-		ii_4 = create(:invoice_item, item_id: 2, invoice_id: 2, quantity: 2)
-		ii_5 = create(:invoice_item, item_id: 3, invoice_id: 2, quantity: 1)
-		ii_6 = create(:invoice_item, item_id: 1, invoice_id: 3, quantity: 3)
-		ii_7 = create(:invoice_item, item_id: 3, invoice_id: 3, quantity: 2)
+		@ii_1 = create(:invoice_item, item_id: 1, invoice_id: 1, quantity: 1)
+		@ii_2 = create(:invoice_item, item_id: 2, invoice_id: 1, quantity: 1)
+		@ii_3 = create(:invoice_item, item_id: 3, invoice_id: 1, quantity: 1)
+		@ii_4 = create(:invoice_item, item_id: 2, invoice_id: 2, quantity: 2)
+		@ii_5 = create(:invoice_item, item_id: 3, invoice_id: 2, quantity: 1)
+		@ii_6 = create(:invoice_item, item_id: 1, invoice_id: 3, quantity: 3)
+		@ii_7 = create(:invoice_item, item_id: 3, invoice_id: 3, quantity: 2)
   end
 
-  it 'sends a list of invoices' do
+  it 'sends a list of invoice items' do
     get '/api/v1/invoice_items.json'
 
     expect(response).to be_success
@@ -21,111 +21,119 @@ RSpec.describe "InvoiceItems API" do
     expect(json.last['item_id']).to eq(ii_7.item_id)
     expect(json.last['invoice_id']).to eq(ii_7.invoice_id)
     expect(json.last['quantity']).to eq(ii_7.quantity)
+		expect(json.last['unit_price']).to eq(ii_7.unit_price)
+
   end
 
-	# it 'sends details on one invoice when passed ID' do
-	# 	get "/api/v1/invoices/#{i_2.id}.json"
-	#
-	# 	expect(response).to be_success
-	# 	expect(json["id"]).to eq(i_2.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-	# end
-	#
-  # it 'sends details on invoice when passed ID as param' do
-	# 	get "/api/v1/invoices/find?id=#{i_2.id}"
-	#
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_2.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-	# end
-	#
-  # it 'sends details on invoice when passed CUSTOMER_ID as param' do
-  #   get "/api/v1/invoices/find?customer_id=#{i_2.customer_id}"
-	#
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_2.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-  # end
-	#
-  # it 'sends details on first matching invoice when passed CUSTOMER_ID as param' do
-  #   i_4 = create(:invoice, customer_id: i_2.customer_id)
-  #   get "/api/v1/invoices/find?customer_id=#{i_2.customer_id}"
-	#
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_2.id)
-  #   expect(json["id"]).to_not eq(i_4.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-  # end
-	#
-  # it 'sends details on invoice when passed MERCHANT_ID as param' do
-  #   get "/api/v1/invoices/find?merchant_id=#{i_2.merchant_id}"
-	#
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_2.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-  # end
-	#
-  # it 'sends details on first matching invoice when passed CUSTOMER_ID as param' do
-  #   i_4 = create(:invoice, merchant_id: i_2.merchant_id)
-  #   get "/api/v1/invoices/find?merchant_id=#{i_2.merchant_id}"
-	#
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_2.id)
-  #   expect(json["id"]).to_not eq(i_4.id)
-  #   expect(json['customer_id']).to eq(i_2.customer_id)
-  #   expect(json['merchant_id']).to eq(i_2.merchant_id)
-  #   expect(json['status']).to eq(i_2.status)
-  # end
-	#
-  # it 'sends details on first matching invoice when passed STATUS as param' do
-  #   get "/api/v1/invoices/find?status=#{i_2.status}"
-	#
-  #   expect(i_1.status).to eq (i_2.status)
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_1.id)
-  #   expect(json["id"]).to_not eq(i_2.id)
-  #   expect(json['customer_id']).to eq(i_1.customer_id)
-  #   expect(json['merchant_id']).to eq(i_1.merchant_id)
-  #   expect(json['status']).to eq(i_1.status)
-  # end
-	#
-  # it 'sends details on invoice when passed STATUS case-insensitive' do
-  #   get "/api/v1/invoices/find?status=sHipped"
-	#
-  #   expect(i_1.status).to eq (i_2.status)
-  #   expect(response).to be_success
-  #   expect(json["id"]).to eq(i_1.id)
-  # end
-	#
-  # it 'sends details on multiple invoices when passed STATUS' do
-  #   i_4 = create(:invoice, status: "cancelled")
-  #   get "/api/v1/invoices/find_all?status=#{i_1.status}"
-	#
-  #   expect(response).to be_success
-  #   expect(json.length).to eq(3)
-  #   expect(json.last['customer_id']).to eq(i_3.customer_id)
-  #   expect(json.last['merchant_id']).to eq(i_3.merchant_id)
-  #   expect(json.last['status']).to eq(i_3.status)
-  # end
-	#
-  # it 'sends details on multiple invoices when passed STATUS' do
-  #   get "/api/v1/invoices/random.json"
-	#
-  #   i_random = Invoice.find(json['id'])
-	#
-  #   expect(response).to be_success
-  #   expect(json['customer_id']).to eq(i_random.customer_id)
-  #   expect(json['merchant_id']).to eq(i_random.merchant_id)
-  #   expect(json['status']).to eq(i_random.status)
-  # end
+	it 'sends details on one invoice item when passed ID' do
+		get "/api/v1/invoice_items/#{ii_2.id}.json"
+
+		expect(response).to be_success
+		expect(json['id']).to eq(ii_2.id)
+    expect(json['item_id']).to eq(ii_2.item_id)
+    expect(json['invoice_id']).to eq(ii_2.invoice_id)
+    expect(json['quantity']).to eq(ii_2.quantity)
+		expect(json['unit_price']).to eq(ii_2.unit_price)
+	end
+
+  it 'sends details on invoice item when passed ID as param' do
+		get "/api/v1/invoice_items/find?id=#{ii_2.id}"
+
+    expect(response).to be_success
+		expect(json['id']).to eq(ii_2.id)
+    expect(json['item_id']).to eq(ii_2.item_id)
+    expect(json['invoice_id']).to eq(ii_2.invoice_id)
+    expect(json['quantity']).to eq(ii_2.quantity)
+		expect(json['unit_price']).to eq(ii_2.unit_price)
+	end
+
+  it 'sends details on first matching invoice when passed ITEM_ID as param' do
+    get "/api/v1/invoice_items/find?item_id=#{ii_2.item_id}"
+
+		expect(response).to be_success
+		expect(json['id']).to eq(ii_2.id)
+    expect(json['item_id']).to eq(ii_2.item_id)
+    expect(json['invoice_id']).to eq(ii_2.invoice_id)
+    expect(json['quantity']).to eq(ii_2.quantity)
+		expect(json['unit_price']).to eq(ii_2.unit_price)
+  end
+
+	it 'sends details on all matching invoices when passed ITEM_ID as param' do
+    get "/api/v1/invoice_items/find_all?item_id=#{ii_2.item_id}"
+
+	  expect(response).to be_success
+	  expect(json.length).to eq(2)
+		expect(json.last['id']).to eq(ii_4.id)
+  end
+
+	it 'sends details on first matching invoice when passed INVOICE_ID as param' do
+		get "/api/v1/invoice_items/find?invoice_id=#{ii_2.invoice_id}"
+
+		expect(ii_2.invoice_id).to eq(ii_1.invoice_id)
+		expect(response).to be_success
+		expect(json['id']).to eq(ii_1.id)
+		expect(json['item_id']).to eq(ii_1.item_id)
+		expect(json['invoice_id']).to eq(ii_1.invoice_id)
+		expect(json['quantity']).to eq(ii_1.quantity)
+		expect(json['unit_price']).to eq(ii_1.unit_price)
+	end
+
+	it 'sends details on all matching invoices when passed INVOICE_ID as param' do
+		get "/api/v1/invoice_items/find_all?invoice_id=#{ii_2.invoice_id}"
+
+		expect(response).to be_success
+		expect(json.length).to eq(3)
+		expect(json.last['id']).to eq(ii_3.id)
+	end
+
+	it 'sends details on first matching invoice when passed QUANTITY as param' do
+		get "/api/v1/invoice_items/find?quantity=#{ii_2.quantity}"
+
+		expect(ii_2.quantity).to eq(ii_1.quantity)
+		expect(response).to be_success
+		expect(json['id']).to eq(ii_1.id)
+		expect(json['item_id']).to eq(ii_1.item_id)
+		expect(json['invoice_id']).to eq(ii_1.invoice_id)
+		expect(json['quantity']).to eq(ii_1.quantity)
+		expect(json['unit_price']).to eq(ii_1.unit_price)
+	end
+
+	it 'sends details on all matching invoices when passed QUANTITY as param' do
+		get "/api/v1/invoice_items/find_all?quantity=#{ii_2.quantity}"
+
+		expect(response).to be_success
+		expect(json.length).to eq(4)
+		expect(json.last['id']).to eq(ii_5.id)
+	end
+
+	it 'sends details on first matching invoice when passed UNIT_PRICE as param' do
+		get "/api/v1/invoice_items/find?unit_price=#{ii_2.unit_price}"
+
+		expect(response).to be_success
+		expect(json['id']).to eq(ii_2.id)
+		expect(json['item_id']).to eq(ii_2.item_id)
+		expect(json['invoice_id']).to eq(ii_2.invoice_id)
+		expect(json['quantity']).to eq(ii_2.quantity)
+		expect(json['unit_price']).to eq(ii_2.unit_price)
+	end
+
+	it 'sends details on all matching invoices when passed UNIT_PRICE as param' do
+		get "/api/v1/invoice_items/find_all?unit_price=#{ii_2.unit_price}"
+
+		expect(response).to be_success
+		expect(json.length).to eq(1)
+		expect(json.last['id']).to eq(ii_2.id)
+	end
+
+  it 'sends details on random invoice when passed RANDOM' do
+    get "/api/v1/invoice_items/random.json"
+
+    ii_random = InvoiceItem.find(json['id'])
+
+    expect(response).to be_success
+		expect(json['item_id']).to eq(ii_random.item_id)
+		expect(json['invoice_id']).to eq(ii_random.invoice_id)
+		expect(json['quantity']).to eq(ii_random.quantity)
+		expect(json['unit_price']).to eq(ii_random.unit_price)
+  end
 end
