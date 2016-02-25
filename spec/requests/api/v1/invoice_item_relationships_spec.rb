adf4718cfa3d601802f9dfec275601fdf8237672
Beth Sebian <esebian@gmail.com>
2016-02-24 21:30:52 -0700

    Finish InvoiceItem relationships tests and controllers

--- spec/requests/api/v1/invoice_item_relationships_spec.rb
+++ spec/requests/api/v1/invoice_item_relationships_spec.rb
@@ -1,45 +1,31 @@
-
-
 require 'rails_helper'
 
-RSpec.describe "Merchant Relationships API" do
-  attr_reader :m_1, :in_1, :in_2, :it_1, :it_2
+RSpec.describe "InvoiceItem Relationships API" do
 
-  before(:each) do
-		@m_1  = create(:merchant)
-    @in_1 = create(:invoice, merchant_id: m_1.id)
-    @in_2 = create(:invoice, merchant_id: m_1.id)
-    @it_1 = create(:item, merchant_id: m_1.id)
-    @it_2 = create(:item, merchant_id: m_1.id)
-  end
+	it 'returns the associated invoice of an invoice item' do
+		invoice = create(:invoice)
+		invoice_item = create(:invoice_item, invoice_id: invoice.id)
 
-  it 'sends a list of merchant items' do
-    get "/api/v1/merchants/#{m_1.id}/items.json"
+		get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
 
-    expect(response).to be_success
-    expect(json.length).to eq(2)
-    expect(json.first['id']).to eq(it_2.id)
-    expect(json.first['name']).to eq(it_2.name)
-		expect(json.last['id']).to eq(it_1.id)
-    expect(json.last['description']).to eq(it_1.description)
-  end
-  #
-	it 'sends a list of merchant invoices' do
-		get "/api/v1/merchants/#{m_1.id}/invoices.json"
+		expect(response).to be_success
+		expect(json['id']).to eq(invoice.id)
+    expect(json['customer_id']).to eq(invoice.customer_id)
+		expect(json['merchant_id']).to eq(invoice.merchant_id)
+		expect(json['status']).to eq(invoice.status)
+	end
 
-    expect(response).to be_success
-    expect(json.length).to eq(2)
-    expect(json.first['id']).to eq(in_2.id)
-    expect(json.first['customer_id']).to eq(in_2.customer_id)
-		expect(json.last['id']).to eq(in_1.id)
-    expect(json.last['merchant_id']).to eq(in_1.merchant_id)
+	it 'returns the associated item of an invoice item' do
+		item = create(:item)
+		invoice_item = create(:invoice_item, item_id: item.id)
+
+		get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+		expect(response).to be_success
+		expect(json['id']).to eq(item.id)
+    expect(json['name']).to eq(item.name)
+		expect(json['description']).to eq(item.description)
+		expect(json['unit_price']).to eq((item.unit_price.to_f/100).to_s)
+		expect(json['merchant_id']).to eq(item.merchant_id)
 	end
 end
-
-
-
-
-
-
-GET /api/v1/invoice_items/:id/invoice returns the associated invoice
-GET /api/v1/invoice_items/:id/item returns the associated item
